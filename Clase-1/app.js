cf169d7c80f3c370c72c29f09c075290c573f784
oscarleon <oscarleon.alcatel@gmai.com>
2024-01-20 11:53:36 -0600

    clase 2

--- Clase-1/app.js
+++ Clase-1/app.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo");
+let cadaDeAndy = "Woody";
+console.log(cadaDeAndy);
+
+// Lo NO permitido
+// Evitar colocar nombres que no son descriptivos, ya que no podremos saber más adelante de que trata la variable.
+
+// Lo PERMITIDO
+// Las variables siempre van al inicio de nuestro código, siempre cerrar con ;
---
+++ Clase-2/app.js
@@ -0,0 +1,15 @@
+// Tipos de datos
+
+// PRIMITIVOS
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+// bigint
+
+// COMPLEJOS
+// object
+// array
+// function
